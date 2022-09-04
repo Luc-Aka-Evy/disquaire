fc712d326746788acbfda2dd084963c6b8d2dfe0
AkaSan242 <Lucaka@icloud.com>
2022-09-04 16:54:52 +0200

    update projects settings and files

--- disquaire_project/urls.py
+++ disquaire_project/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path as url
 from django.contrib import admin
 
 from store import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^store/', include('store.urls', namespace='store')),
+    url(r'^store/', include(('store.urls', 'store'), namespace='store')),
     url(r'^admin/', admin.site.urls)
 ]
 
